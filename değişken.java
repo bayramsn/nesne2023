22a4400b9fdddb1e44877cef733d9f1da27cc8e8
bayramsn <116954592+bayramsn@users.noreply.github.com>
2023-11-07 18:13:21 +0300

    değişken ders 1
    bakın

---
+++ değişken.java
@@ -0,0 +1,28 @@
+public class değişken {
+
+    public static void main(String[] args) {
+        System.out.println(5*5);
+int a=5;
+int b=6;
+int c=7;
+double pi = 3.14;
+float d = 3.14f;
+String name ="bayram";
+String surname="kaya";
+System.out.println(name+" "+surname);
+
+
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(a+b+c);
+if (a>b) { System.out.println("a büyüktür b"); }
+else if (a==b) { System.out.println("a eşittir b"); }
+else; { System.out.println("a küçüktür b"); }
+    
+ 
+
+    }
+}
+
